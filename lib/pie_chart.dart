6efcc6a776806ca6b19c3b38e653d67cf2c80b88
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2024-09-23 17:03:47 +0200

    pie charts: add legend and tooltip, sort items, group other items

--- lib/pie_chart.dart
+++ lib/pie_chart.dart
@@ -5,9 +5,16 @@ import 'package:swift_charts/swift_charts.dart';
 
 class PieChartItem {
   String label;
+  String shortLabel;
   double weight;
   String? color;
-  PieChartItem(this.label, this.weight, {this.color});
+  bool isActive = false;
+  static int maxLabelLength = 14;
+  String? othersText = null;
+
+  PieChartItem(this.label, this.weight, {this.color}):
+    shortLabel = (label.length > maxLabelLength) ? label.substring(0, maxLabelLength - 2) + '..' : label {
+  }
 }
 /**
  * Simple PieChart
@@ -17,63 +24,194 @@ class SwiftPieChart extends SwiftChart<List<PieChartItem>> {
   List<PieChartItem> items = [];
   CanvasElement canvas;
   DivElement container;
+  bool legend;
+  DivElement canvasTip;
 
-  SwiftPieChart(this.container) :
-        canvas = new CanvasElement()
-  {
-    container.append(canvas);
-    canvas.style.width='100%';
-    canvas.style.height='200px';
-    renderText('rendering...');
-  }
+  static int maxLabels = 14;
 
-  List<String> _colors = [
-    'rgb(202, 175, 123)',
-    'rgb(208, 136, 113)',
-    'rgb(180, 135, 113)',
-    'rgb(216, 181, 105)',
-    'rgb(198, 116, 103)',
-    'rgb(206, 161, 105)',
-    'rgb(220, 175, 143)'
-  ];
-
-  setColors(List<String> colors) {
-    _colors = colors;
-  }
-
-  render() {
-    var ctx = this.startRender();
-    double start = 0;
-    double total = 0;
-    var c = 0;
-    for (var i = 0; i < items.length; i++) {
-      total += items[i].weight;
+  setData(List<PieChartItem> items) {
+    items.sort((a, b) => -a.weight.compareTo(b.weight));
+    totalWeight = items.map((item) => item.weight).reduce((a, b) => a + b);
+    if (items.length > maxLabels) {
+      var lefts = items.sublist(maxLabels - 1);
+      items = items.sublist(0, maxLabels - 1);
+      var leftWeight = lefts.map((i) => i.weight).reduce((a, b) => a + b);
+      items.add(
+          PieChartItem('other..', leftWeight)
+            ..othersText = lefts.map((i) => (100 * i.weight / totalWeight).toStringAsFixed(2) + '% ' + i.label).join('<br/>')
+      );
     }
+    var c = 0;
     for (var i = 0; i < items.length; i++) {
       if (i == items.length - 1 && c == 0) {
         //make sure last color is different than first
         c = 1;
       }
-      if (items[i].color == null) {
-        items[i].color = _colors[c];
-      }
-      drawSegment(ctx, items[i], start, total);
+      items[i].color = _colors[c];
       c++;
       if (c >= _colors.length) {
         c = 0;
+      }
+    }
+    super.setData(items);
+  }
+
+  SwiftPieChart(this.container, { this.legend = false }) :
+        canvas = new CanvasElement(),
+        canvasTip = new DivElement()
+  {
+    container.append(canvas);
+    canvas.style.width='100%';
+    canvas.style.height='280px';
+    canvas.onMouseMove.listen(handleMouseMove);
+    canvas.onMouseLeave.listen(handleMouseLeave);
+    renderText('rendering...');
+    container.append(canvasTip);
+    container.style.position = 'relative';
+
+    canvasTip.style.position = 'absolute';
+    canvasTip.style.border = '1px solid gray';
+    canvasTip.style.background = 'white';
+    canvasTip.style.minWidth = '100px';
+    canvasTip.style.maxWidth = '200px';
+    canvasTip.style.overflow = 'hidden';
+    canvasTip.style.fontSize = '10px';
+    canvasTip.style.padding = '5px';
+    canvasTip.style.display = 'none';
+  }
+
+  List<String> _colors = [
+    '#c472e8',
+    '#ff8d72',
+    '#f76ad1',
+    '#ffab55',
+    '#ff69b3',
+    '#ffc940',
+    '#ff7692',
+    '#ffe640',
+  ];
+
+
+  void handleMouseLeave(MouseEvent event) {
+    for (var item in items) {
+      item.isActive = false;
+    }
+    canvasTip.style.display = 'none';
+    render();
+  }
+
+  void handleMouseMove(MouseEvent event){
+    var rect = (event.target as Element).getBoundingClientRect();
+    var x = event.client.x - rect.left; //x position within the element.
+    var y = event.client.y - rect.top;  //y position within the element.
+
+    var rerender = false;
+    PieChartItem? currentItem = null;
+    var radius = (min(width, height) / 2) * 0.9;
+
+    if ((x > width - legendWidth) && (y > height - legendHeight)) {
+
+      num ly = (y - (height - legendHeight));
+      for (var item in items) {
+        bool newIsActive = ly > 0 && ly < 20;
+        if (item.isActive != newIsActive) {
+          rerender = true;
+        }
+        item.isActive = newIsActive;
+        if (item.isActive) {
+          currentItem = item;
+        }
+        ly -= 20;
+      }
+    } else if (sqrt(pow(y - centerY, 2) + pow(x - centerX, 2)) < radius) {
+      var currentAngle = atan2(y - centerY, x - centerX) + (pi / 2);
+      if (currentAngle < 0) {
+        currentAngle = currentAngle + (2 * pi);
+      }
+      double startingAngle = 0;
+      for (var item in items) {
+        var arcSize = degreesToRadians((item.weight / totalWeight) * 360);
+        bool newIsActive = (startingAngle < currentAngle &&
+            (startingAngle + arcSize) > currentAngle);
+        if (item.isActive != newIsActive) {
+          rerender = true;
+        }
+        item.isActive = newIsActive;
+        if (item.isActive) {
+          currentItem = item;
+        }
+        startingAngle += arcSize;
+      }
+    } else {
+      for (var item in items) {
+        if (item.isActive) {
+          rerender = true;
+        }
+        item.isActive = false;
+      }
+    }
+    canvasTip.innerHtml = '<strong>' + (currentItem?.label ?? '') + '</strong><br/>' + (currentItem?.othersText ?? ((currentItem?.weight ?? 0) * 100 / totalWeight).toStringAsFixed(2) + '%');
+    canvasTip.style.display = currentItem != null ? 'block' : 'none';
+
+    canvasTip.style.right = (x < width / 2) ? '0' : 'auto';
+    canvasTip.style.left = (x >= width / 2) ? '0' : 'auto';
+    canvasTip.style.bottom = (y < height / 2) ? '0' : 'auto';
+    canvasTip.style.top = (y >= height / 2) ? '0' : 'auto';
+
+    if (rerender) {
+      render();
+    }
+  }
+
+  setColors(List<String> colors) {
+    _colors = colors;
+  }
+
+  String get legendFont => "10pt Helvetica";
+
+  double totalWeight = 0;
+  render() {
+    var ctx = this.startRender();
+    legendWidth = 0;
+
+    ctx.font = legendFont;
+    items.map((item) => ctx.measureText(legendLabel(item)).width).forEach((a) {
+      if (a! > legendWidth) {
+        legendWidth = a.ceil();
+      }
+    });
+    legendWidth += 30;
+
+    double start = 0;
+    for (var i = 0; i < items.length; i++) {
+      if (!items[i].isActive) {
+        drawSegment(ctx, i, items[i], start);
+      }
+      start += items[i].weight;
+    }
+    //draw active items over inactive
+    start = 0;
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].isActive) {
+        drawSegment(ctx, i, items[i], start);
       }
       start += items[i].weight;
     }
   }
 
-  drawSegment(CanvasRenderingContext2D ctx, PieChartItem item, double start, double total) {
-    ctx.save();
-    var centerX = (width / 2).floor();
-    var centerY = (height / 2).floor();
+  int get centerX => legend ? ((width - legendWidth) / 2).floor() : (width / 2).floor();
+  int get centerY => (height / 2).floor();
 
-    var radius = (min(width, height) / 2) * 0.8;
-    var startingAngle = degreesToRadians((start / total) * 360 - 90.0);
-    var arcSize = degreesToRadians((item.weight / total) * 360);
+  int get legendHeight => 20 * items.length;
+  int legendWidth = 0;
+
+
+  drawSegment(CanvasRenderingContext2D ctx, int idx, PieChartItem item, double start) {
+    ctx.save();
+
+    var radius = (min(width, height) / 2) * (item.isActive ? 0.88 : 0.82);
+    var startingAngle = degreesToRadians((start / totalWeight) * 360 - 90.0) - (item.isActive ? 0.03 : 0);
+    var arcSize = degreesToRadians((item.weight / totalWeight) * 360) + (item.isActive ? 0.06 : 0);
     var endingAngle = startingAngle + arcSize;
 
     ctx.beginPath();
@@ -86,30 +224,51 @@ class SwiftPieChart extends SwiftChart<List<PieChartItem>> {
     ctx.fill();
     ctx.restore();
 
-    this.drawSegmentLabel(ctx, item, start, total);
+    this.drawSegmentLabel(ctx, idx, item, start);
   }
 
   double degreesToRadians(double degrees) {
     return (degrees * pi) / 180;
   }
 
-  drawSegmentLabel(CanvasRenderingContext2D ctx, PieChartItem item, double start, double total) {
-    ctx.save();
-    var x = (width / 2).floor();
-    var y = (height / 2).floor();
-    var angle = this.degreesToRadians((start / total) * 360 - 90);
+  String legendLabel(PieChartItem item) {
+    return (item.weight * 100 / totalWeight).round().toString() + '% ' + item.shortLabel;
+  }
 
-    ctx.translate(x, y);
+  drawSegmentLabel(CanvasRenderingContext2D ctx, int idx, PieChartItem item, double start) {
+    ctx.save();
+    var angle = this.degreesToRadians((start / totalWeight) * 360 - 90);
+
+    ctx.translate(centerX, centerY);
     ctx.rotate(angle);
-    var dx = (min(width, height) * 0.5).floor() * 0.8 - 10;
+    var dx = (min(width, height) * 0.5).floor() * 0.8 - 5;
     var dy = (height * 0.05).floor();
 
     ctx.textAlign = "right";
-    int fontSize = (height / 25).floor();
-    ctx.font = fontSize.toString() + "pt Helvetica";
+    int fontSize = min(11, (height / 25)).floor();
+    ctx.font = (item.isActive ? "bold " : '') + fontSize.toString() + "pt Helvetica";
 
-    ctx.fillText(item.label, dx, dy);
+    ctx.fillText(item.shortLabel, dx, dy);
 
     ctx.restore();
+
+    ctx.fillStyle = item.color;
+    //ctx.fillRect(width - legendWidth, height - legendHeight, legendWidth, legendHeight);
+    //ctx.fillText(item.label, 10, 10);
+    int legendSize = 5;
+    if (legend) {
+
+      int x = width - legendWidth + 5;
+      int y = height - legendHeight + (idx * 20) + 10;
+      ctx.fillStyle = item.color;
+      legendSize = item.isActive ? 7 : 5;
+      ctx.fillRect(x - legendSize, y - legendSize, 2 * legendSize, 2 * legendSize);
+
+      ctx.textAlign = "left";
+      ctx.fillStyle = 'black';
+      ctx.font = (item.isActive ? "bold " : '') + legendFont;
+
+      ctx.fillText(legendLabel(item), x + 10, y + 4);
+    }
   }
 }
